fe03624b16fe4692b8ec39b16b125d5255ad1b28
Sam Fentress <sfentress@concord.org>
2012-10-18 12:12:31 -0400

    Refactor into OO design with CiSo top-level object
    New circuits are created by creating a new CiSo object, 
    instead of everything being global.
    
    Refactor some ownership of functions, such as which class 
    is responsible for, e.g. getNodeIndexes(), in general 
    shifting all circuit-domain knowledge to CiSo alone, so 
    that Nodes and Components know nothing about the circuit.

--- src/circuitSolver.js
+++ src/circuitSolver.js
@@ -1,160 +1,166 @@
 
-var components = new Array;
-var dcVoltageSources = new Array;
-var acVoltageSources = new Array;
-var wires = new Array;
-var circuitNodeLabels = new Array;
-var circuitNodes = [];
-var twoPi = 2*Math.PI;
-var gMatrix = [];
+CiSo = {};
 
+(function(){
 
+	CiSo = function() {
+		this.components = new Array;
+		this.dcVoltageSources = new Array;
+		this.acVoltageSources = new Array;
+		this.wires = new Array;
+		this.circuitNodeLabels = new Array;
+		this.circuitNodes = [];
+		this.gMatrix = [];
+	}
 
-Node.prototype.getLinkedComponents = function () {
+	CiSo.prototype.getLinkedComponents = function (node) {
 		var links = [];
-		for (var i = components.length - 1; i >= 0; i--) {
-			 if((this.label === components[i].nodeLabels[0]) || (this.label === components[i].nodeLabels[1])) {
-			 	links.push(components[i]);
+		for (var i = this.components.length - 1; i >= 0; i--) {
+			 if((node.label === this.components[i].nodeLabels[0]) || (node.label === this.components[i].nodeLabels[1])) {
+			 	links.push(this.components[i]);
 			 }
 		}
 		return links;
 	}
 
-Component.prototype.getImpedance = function(frequency) {
-    var impedance = new Complex (0,0)
-    if (this.type === "Resistor") {
-    	impedance.real = this.value;
-    	impedance.imag = 0;
-    }
-    else if (this.type == "Capacitor") {
-		impedance.real = 0;
-		impedance.imag = 1/(twoPi * frequency * this.value);
+	CiSo.prototype.getDiagonalMatrixElement = function(node, freq) {
+		var neighborComponents = this.getLinkedComponents(node);
+		var matrixElement = new Complex(0,0);
+		for (var i = neighborComponents.length - 1; i >= 0; i--) {
+		    var imp = neighborComponents[i].getImpedance(freq);
+			matrixElement = matrixElement.add(imp.inverse());
+		};
+		return matrixElement;
 	}
-    else if (this.type == "Inductor") {
-		impedance.real = 0;
-		impedance.imag = twoPi * frequency * this.value;
+
+	CiSo.prototype.getNodeIndexes = function(component) {
+		var indexes = new Array(2);
+		indexes[0] = this.circuitNodeLabels.indexOf(component.nodeLabels[0]);
+		indexes[1] = this.circuitNodeLabels.indexOf(component.nodeLabels[1]);
+		return indexes;
 	}
-	return impedance;
-}
 
-Component.prototype.getNodeIndexes = function() {
-	var myIndexes = new Array(2);
-	myIndexes[0] = circuitNodeLabels.indexOf(this.nodeLabels[0]);
-	myIndexes[1] = circuitNodeLabels.indexOf(this.nodeLabels[1]);
-	return myIndexes;
-}
+	Node = function(label) {
+		this.label = label;
+	}
 
-Component.prototype.getOffDiagonalMatrixElement = function(freq) {
-	return this.getImpedance(freq).inverse();
-}
+	Component = function(label, type, value, nodeLabels) {
+		this.label = label;
+		this.type = type;
+		this.value = value;
+		this.nodeLabels = nodeLabels;
+	}
 
-function Node(label) {
-	this.label = label;
-	this.index = circuitNodes.indexOf(this);
-}
+	twoPi = 2*Math.PI;
 
-Node.prototype.getDiagonalMatrixElement = function(freq) {
-	var neighborComponents = this.getLinkedComponents();
-	var matrixElement = new Complex(0,0);
-	for (var i = neighborComponents.length - 1; i >= 0; i--) {
-	    var imp = neighborComponents[i].getImpedance(freq);
-		matrixElement = matrixElement.add(imp.inverse());
-	};
-	return matrixElement;
-}
-
-function Component(label, type, value, nodeLabels) {
-	this.label = label;
-	this.type = type;
-	this.value = value;
-	this.nodeLabels = nodeLabels;
-}
-
-function DCVoltageSource(label,voltage,pos_node,neg_node){
-	this.label = label
-	this.voltage = voltage;
-	this.pos_node = pos_node;
-	this.neg_node = neg_node;
-}
-
-function ACVoltageSource(label,voltage,groundNodeLabel,voltageNodeLabel,frequency){
-	this.voltage = voltage;
-	this.groundNodeLabel = groundNodeLabel;
-	this.voltageNodeLabel = voltageNodeLabel;
-	this.frequency = frequency;
-}
-
-function addComponent(label,type,value,nodeLabels) {
-  var component = new Component(label,type,value,nodeLabels); // Make a new component with the right properties
-  components.push(component); // Push the new component onto the components array.
-}
-
-function addDCVoltageSource(label,voltage,positiveNodeLabel,negativeNodeLabel) {
-  var source = new DCVoltageSource(label,voltage,positiveNodeLabel,negativeNodeLabel);
-  dcVoltageSources.push(source);
-}
-
-function addACVoltageSource(label,voltage,groundNodeLabel,voltageNodeLabel,frequency) {
-  var source = new ACVoltageSource(label,voltage,groundNodeLabel,voltageNodeLabel,frequency);
-  acVoltageSources.push(source);
-}
-
-function makeNodeList() { // Query each component for its nodes, add them to the list if they're not already on it
-// Nodes with the same label are considered to be the same node, so we have to look at the labels to determine uniqueness.
-	for (var i = 0; i < components.length; i++) {
-		var tempNodeLabelArray = components[i].nodeLabels; // array of the labels of both nodes owned by the component
-		var tempNodeLabel1 = tempNodeLabelArray[0];
-		var tempNodeLabel2 = tempNodeLabelArray[1];
-		if (circuitNodeLabels.indexOf(tempNodeLabel1) === -1) { // if the label is not already on the list
-			circuitNodeLabels.push(tempNodeLabel1); // push it onto the label list
-		    var tempNode1 = new Node(tempNodeLabel1); // make a node with that label
-		    circuitNodes.push(tempNode1); // and push it onto the node list
+	Component.prototype.getImpedance = function(frequency) {
+	    var impedance = new Complex (0,0)
+	    if (this.type === "Resistor") {
+	    	impedance.real = this.value;
+	    	impedance.imag = 0;
+	    }
+	    else if (this.type == "Capacitor") {
+			impedance.real = 0;
+			impedance.imag = 1/(twoPi * frequency * this.value);
 		}
-		if (circuitNodeLabels.indexOf(tempNodeLabel2) === -1)  {
-			circuitNodeLabels.push(tempNodeLabel2);
-		    var tempNode2 = new Node(tempNodeLabel2);
-			circuitNodes.push(tempNode2);
+	    else if (this.type == "Inductor") {
+			impedance.real = 0;
+			impedance.imag = twoPi * frequency * this.value;
 		}
+		return impedance;
 	}
-}
 
-function fillGMatrix(){
-	if (acVoltageSources.length > 0){
-		var source = acVoltageSources[0];
-		var frequency = source.frequency;
+	Component.prototype.getOffDiagonalMatrixElement = function(freq) {
+		return this.getImpedance(freq).inverse();
 	}
-	var cZero = new Complex (0,0);
-	for (var i = 0; i < circuitNodes.length + 1; i++) {
-		gMatrix [i] = new Array (circuitNodes.length + 1);
+
+	DCVoltageSource = function (label,voltage,pos_node,neg_node){
+		this.label = label
+		this.voltage = voltage;
+		this.pos_node = pos_node;
+		this.neg_node = neg_node;
 	}
-	for (var i = 0; i < circuitNodes.length + 1; i++) {
-		for (var j = 0; j < circuitNodes.length + 1; j++) {
-			gMatrix[i][j] = cZero;
+
+	ACVoltageSource = function (label,voltage,groundNodeLabel,voltageNodeLabel,frequency){
+		this.voltage = voltage;
+		this.groundNodeLabel = groundNodeLabel;
+		this.voltageNodeLabel = voltageNodeLabel;
+		this.frequency = frequency;
+	}
+
+	CiSo.prototype.addComponent = function (label,type,value,nodeLabels) {
+	  var component = new Component(label,type,value,nodeLabels); // Make a new component with the right properties
+	  this.components.push(component); // Push the new component onto the components array.
+	}
+
+	CiSo.prototype.addDCVoltageSource = function (label,voltage,positiveNodeLabel,negativeNodeLabel) {
+	  var source = new DCVoltageSource(label,voltage,positiveNodeLabel,negativeNodeLabel);
+	  this.dcVoltageSources.push(source);
+	}
+
+	CiSo.prototype.addACVoltageSource = function (label,voltage,groundNodeLabel,voltageNodeLabel,frequency) {
+	  var source = new ACVoltageSource(label,voltage,groundNodeLabel,voltageNodeLabel,frequency);
+	  this.acVoltageSources.push(source);
+	}
+
+	CiSo.prototype.makeNodeList = function () { // Query each component for its nodes, add them to the list if they're not already on it
+	// Nodes with the same label are considered to be the same node, so we have to look at the labels to determine uniqueness.
+	var components = this.components;
+		for (var i = 0; i < components.length; i++) {
+			var tempNodeLabelArray = components[i].nodeLabels; // array of the labels of both nodes owned by the component
+			var tempNodeLabel1 = tempNodeLabelArray[0];
+			var tempNodeLabel2 = tempNodeLabelArray[1];
+			if (this.circuitNodeLabels.indexOf(tempNodeLabel1) === -1) { // if the label is not already on the list
+				this.circuitNodeLabels.push(tempNodeLabel1); // push it onto the label list
+			    var tempNode1 = new Node(tempNodeLabel1); // make a node with that label
+			    this.circuitNodes.push(tempNode1); // and push it onto the node list
+			}
+			if (this.circuitNodeLabels.indexOf(tempNodeLabel2) === -1)  {
+				this.circuitNodeLabels.push(tempNodeLabel2);
+			    var tempNode2 = new Node(tempNodeLabel2);
+				this.circuitNodes.push(tempNode2);
+			}
 		}
 	}
-	for (var i = 0; i < circuitNodes.length; i++){
-		gMatrix[i][i] = circuitNodes[i].getDiagonalMatrixElement(frequency);
-	}
-	for (var i = 0; i < components.length; i++) {
-	 var rowIndex = components[i].getNodeIndexes()[0];
-	 var colIndex = components[i].getNodeIndexes()[1];
-	 gMatrix[rowIndex][colIndex] = components[i].getOffDiagonalMatrixElement(frequency);
-	}
-}
 
-function augmentGMatrix(){
-	if (acVoltageSources.length > 0){
-		var one = new Complex (1,0);
-		var source = acVoltageSources[0];
-		var frequency = source.frequency;
-		var voltageNodeLabel = source.voltageNodeLabel;
-		var groundNodeLabel = source.groundNodeLabel;
-		var voltageNodeIndex = circuitNodeLabels.indexOf(voltageNodeLabel);
-		var groundNodeIndex = circuitNodeLabels.indexOf(groundNodeLabel);
-		gMatrix[circuitNodes.length][voltageNodeIndex] = one;
-		gMatrix[voltageNodeIndex][circuitNodes.length] = one;
+	CiSo.prototype.fillGMatrix = function (){
+		if (this.acVoltageSources.length > 0){
+			var source = this.acVoltageSources[0];
+			var frequency = source.frequency;
+		}
+		var cZero = new Complex (0,0);
+		for (var i = 0; i < this.circuitNodes.length + 1; i++) {
+			this.gMatrix [i] = new Array (this.circuitNodes.length + 1);
+		}
+		for (var i = 0; i < this.circuitNodes.length + 1; i++) {
+			for (var j = 0; j < this.circuitNodes.length + 1; j++) {
+				this.gMatrix[i][j] = cZero;
+			}
+		}
+		for (var i = 0; i < this.circuitNodes.length; i++){
+			this.gMatrix[i][i] = this.getDiagonalMatrixElement(this.circuitNodes[i], frequency);
+		}
+		for (var i = 0; i < this.components.length; i++) {
+		 var rowIndex = this.getNodeIndexes(this.components[i])[0];
+		 var colIndex = this.getNodeIndexes(this.components[i])[1];
+		 this.gMatrix[rowIndex][colIndex] = this.components[i].getOffDiagonalMatrixElement(frequency);
+		}
 	}
-}
+
+	CiSo.prototype.augmentGMatrix = function (){
+		if (this.acVoltageSources.length > 0){
+			var one = new Complex (1,0);
+			var source = this.acVoltageSources[0];
+			var frequency = source.frequency;
+			var voltageNodeLabel = source.voltageNodeLabel;
+			var groundNodeLabel = source.groundNodeLabel;
+			var voltageNodeIndex = this.circuitNodeLabels.indexOf(voltageNodeLabel);
+			var groundNodeIndex = this.circuitNodeLabels.indexOf(groundNodeLabel);
+			this.gMatrix[this.circuitNodes.length][voltageNodeIndex] = one;
+			this.gMatrix[voltageNodeIndex][this.circuitNodes.length] = one;
+		}
+	}
+})();
 
 
 /*
--- test/spec/CircuitSolverSpec.js
+++ test/spec/CircuitSolverSpec.js
@@ -1,12 +1,14 @@
 describe("CircuitSolver", function() {
 
+  var ciso = new CiSo();
+
   it("We can add a component", function() {
-    addComponent("R1", "Resistor", 5000, ["n1", "n2"]);
-    numComponents = components.length;
-    firstComponentLabel = components[0].label;
-    firstComponentType = components[0].type;
-    firstComponentValue = components[0].value;
-    firstComponentNodes = components[0].nodeLabels;
+    ciso.addComponent("R1", "Resistor", 5000, ["n1", "n2"]);
+    numComponents = ciso.components.length;
+    firstComponentLabel = ciso.components[0].label;
+    firstComponentType = ciso.components[0].type;
+    firstComponentValue = ciso.components[0].value;
+    firstComponentNodes = ciso.components[0].nodeLabels;
     expect(numComponents).toBe(1);
     expect(firstComponentLabel).toBe("R1");
     expect(firstComponentType).toBe("Resistor");
@@ -17,12 +19,12 @@ describe("CircuitSolver", function() {
 
 
  it("We can add a second component", function() {
-    addComponent("C1", "Capacitor", 0.000001, ["n2", "n3"]);
-    numComponents = components.length;
-    secondComponentLabel = components[1].label;
-    secondComponentType = components[1].type;
-    secondComponentValue = components[1].value;
-    secondComponentNodes = components[1].nodeLabels;
+    ciso.addComponent("C1", "Capacitor", 0.000001, ["n2", "n3"]);
+    numComponents = ciso.components.length;
+    secondComponentLabel = ciso.components[1].label;
+    secondComponentType = ciso.components[1].type;
+    secondComponentValue = ciso.components[1].value;
+    secondComponentNodes = ciso.components[1].nodeLabels;
     expect(numComponents).toBe(2);
     expect(secondComponentLabel).toBe("C1");
     expect(secondComponentType).toBe("Capacitor");
@@ -32,12 +34,12 @@ describe("CircuitSolver", function() {
   })
 
   it("We can add a third component", function() {
-    addComponent("L1", "Inductor", 0.00002, ["n3", "n4"]);
-    numComponents = components.length;
-    thirdComponentLabel = components[2].label;
-    thirdComponentType = components[2].type;
-    thirdComponentValue = components[2].value;
-    thirdComponentNodes = components[2].nodeLabels;
+    ciso.addComponent("L1", "Inductor", 0.00002, ["n3", "n4"]);
+    numComponents = ciso.components.length;
+    thirdComponentLabel = ciso.components[2].label;
+    thirdComponentType = ciso.components[2].type;
+    thirdComponentValue = ciso.components[2].value;
+    thirdComponentNodes = ciso.components[2].nodeLabels;
     expect(numComponents).toBe(3);
     expect(thirdComponentLabel).toBe("L1");
     expect(thirdComponentType).toBe("Inductor");
@@ -47,34 +49,34 @@ describe("CircuitSolver", function() {
   })
 
  it("We can add an AC voltage source", function() {
-    addACVoltageSource("ACV1",15,"n1","n4",2000);
-    expect(acVoltageSources.length).toBe(1);
-    expect(acVoltageSources[0].voltage).toBe(15);
-    expect(acVoltageSources[0].groundNodeLabel).toBe("n1");
-    expect(acVoltageSources[0].voltageNodeLabel).toBe("n4");
-    expect(acVoltageSources[0].frequency).toBe(2000);
+    ciso.addACVoltageSource("ACV1",15,"n1","n4",2000);
+    expect(ciso.acVoltageSources.length).toBe(1);
+    expect(ciso.acVoltageSources[0].voltage).toBe(15);
+    expect(ciso.acVoltageSources[0].groundNodeLabel).toBe("n1");
+    expect(ciso.acVoltageSources[0].voltageNodeLabel).toBe("n4");
+    expect(ciso.acVoltageSources[0].frequency).toBe(2000);
   })
 
 it("We can make a node list", function() {
-     makeNodeList();
-     expect(circuitNodes.length).toBe(4);
-     expect(circuitNodeLabels[1]).toBe("n2");
-     expect(circuitNodeLabels[2]).toBe("n3");
+     ciso.makeNodeList();
+     expect(ciso.circuitNodes.length).toBe(4);
+     expect(ciso.circuitNodeLabels[1]).toBe("n2");
+     expect(ciso.circuitNodeLabels[2]).toBe("n3");
   })
 
 it("We can find the components that are linked to a node", function() {
-    var testNode = circuitNodes[2];
+    var testNode = ciso.circuitNodes[2];
     expect(testNode.label).toBe("n3");
-    expect(testNode.getLinkedComponents()).toExist();
-    expect(testNode.getLinkedComponents().length).toBe(2);
-    expect(testNode.getLinkedComponents()[0].label).toBe("L1");
+    expect(ciso.getLinkedComponents(testNode)).toExist();
+    expect(ciso.getLinkedComponents(testNode).length).toBe(2);
+    expect(ciso.getLinkedComponents(testNode)[0].label).toBe("L1");
 })
 
 it("We can compute the complex impedance of a component", function() {
-    var testComponent0 = components[0];
-    var testComponent1 = components[1];
-    var testComponent2 = components[2];
-    var frequency = acVoltageSources[0].frequency;
+    var testComponent0 = ciso.components[0];
+    var testComponent1 = ciso.components[1];
+    var testComponent2 = ciso.components[2];
+    var frequency = ciso.acVoltageSources[0].frequency;
     expect(testComponent0.getImpedance(frequency).real).toBe(5000);
     expect(testComponent0.getImpedance(frequency).imag).toBe(0);
     expect(testComponent1.getImpedance(frequency).real).toBe(0);
@@ -84,46 +86,46 @@ it("We can compute the complex impedance of a component", function() {
 })
 
 it("We can compute the diagonal matrix element for a node", function() {
-    var testNode = circuitNodes[1];
-    var frequency = acVoltageSources[0].frequency;
-    expect(testNode.getDiagonalMatrixElement(frequency).real).toBeCloseTo(0.0002);
-    expect(testNode.getDiagonalMatrixElement(frequency).imag).toBeCloseTo(-0.0125664);
-    testNode = circuitNodes[2];
-    expect(testNode.getDiagonalMatrixElement(frequency).real).toBeCloseTo(0);
-    expect(testNode.getDiagonalMatrixElement(frequency).imag).toBeCloseTo(-3.9914);
+    var testNode = ciso.circuitNodes[1];
+    var frequency = ciso.acVoltageSources[0].frequency;
+    expect(ciso.getDiagonalMatrixElement(testNode, frequency).real).toBeCloseTo(0.0002);
+    expect(ciso.getDiagonalMatrixElement(testNode, frequency).imag).toBeCloseTo(-0.0125664);
+    testNode = ciso.circuitNodes[2];
+    expect(ciso.getDiagonalMatrixElement(testNode, frequency).real).toBeCloseTo(0);
+    expect(ciso.getDiagonalMatrixElement(testNode, frequency).imag).toBeCloseTo(-3.9914);
 })
 
 it("We can compute the off-diagonal matrix element for a component", function() {
-    var frequency = acVoltageSources[0].frequency;
-    var testComponent = components[0];
+    var frequency = ciso.acVoltageSources[0].frequency;
+    var testComponent = ciso.components[0];
     expect(testComponent.getOffDiagonalMatrixElement(frequency).real).toBeCloseTo(0.0002);
     expect(testComponent.getOffDiagonalMatrixElement(frequency).imag).toBeCloseTo(0);
-    testComponent = components[1]
+    testComponent = ciso.components[1]
     expect(testComponent.getOffDiagonalMatrixElement(frequency).real).toBeCloseTo(0);
     expect(testComponent.getOffDiagonalMatrixElement(frequency).imag).toBeCloseTo(-0.0125664);
 })
 
 it("We can fill the G matrix", function() {
-    fillGMatrix();
-    expect (gMatrix[0][0].real).toBe(1/5000);
-    expect (gMatrix[0][0].imag).toBe(0);
-    expect (gMatrix[0][1].real).toBe(.0002);
-    expect (gMatrix[0][1].imag).toBe(0);
-    expect (gMatrix[1][1].real).toBe(.0002);
-    expect (gMatrix[1][1].imag).toBeCloseTo(-0.0125664);
+    ciso.fillGMatrix();
+    expect (ciso.gMatrix[0][0].real).toBe(1/5000);
+    expect (ciso.gMatrix[0][0].imag).toBe(0);
+    expect (ciso.gMatrix[0][1].real).toBe(.0002);
+    expect (ciso.gMatrix[0][1].imag).toBe(0);
+    expect (ciso.gMatrix[1][1].real).toBe(.0002);
+    expect (ciso.gMatrix[1][1].imag).toBeCloseTo(-0.0125664);
 
 })
 
 it("We can augment the G matrix", function() {
-    augmentGMatrix();
-    expect (gMatrix[4][0].real).toBe(0);
-    expect (gMatrix[4][0].imag).toBe(0);
-    expect (gMatrix[4][3].real).toBe(1);
-    expect (gMatrix[4][3].imag).toBe(0);
-    expect (gMatrix[3][4].real).toBe(1);
-    expect (gMatrix[3][4].imag).toBe(0);
-    expect (gMatrix[4][4].real).toBe(0);
-    expect (gMatrix[4][4].imag).toBe(0);
+    ciso.augmentGMatrix();
+    expect (ciso.gMatrix[4][0].real).toBe(0);
+    expect (ciso.gMatrix[4][0].imag).toBe(0);
+    expect (ciso.gMatrix[4][3].real).toBe(1);
+    expect (ciso.gMatrix[4][3].imag).toBe(0);
+    expect (ciso.gMatrix[3][4].real).toBe(1);
+    expect (ciso.gMatrix[3][4].imag).toBe(0);
+    expect (ciso.gMatrix[4][4].real).toBe(0);
+    expect (ciso.gMatrix[4][4].imag).toBe(0);
 })
 
 })
